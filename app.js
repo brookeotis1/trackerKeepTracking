036aaa71a13da9217ce27c060f8c4b9ff15c91c0
Brooke Maneotis <xmaneotisb@yahoo.com>
2020-12-06 14:27:53 -0700

    adding code

---
+++ app.js
@@ -0,0 +1,160 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // port
+  port: 3306,
+
+  // username
+  user: "root",
+
+  // password
+  password: Password1,
+  database: "track_empDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    runSearch();
+  });
+  
+
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "Add new employee",
+          "Add new department",
+          "Add employee new role",
+          "View departments",
+          "View roles",
+          "View employees",
+          "Update empoloyee role",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Add new employee":
+          addEmployee();
+          break;
+  
+        case "Add new department":
+          addDepartment();
+          break;
+  
+        case "Add employee new role":
+          addRole();
+          break;
+  
+        case "View departments":
+          viewDepartments();
+          break;
+              
+        case "View roles":
+          viewRole();
+          break;
+          
+        case "View employees":
+          viewEmployee();
+          break;  
+            
+        case "Update employee role":
+          updateEmployee();
+          break; 
+  
+        case "exit":
+          connection.end();
+          break;
+        }
+      });
+    }
+
+//add employee
+
+function addEmployee() {
+    inquirer
+    .prompt([
+        {
+            name: first,
+            type: "input",
+            message: "Enter first name"
+        },
+        {
+            name: last,
+            type: "input",
+            message: "Enter last name"
+        },
+        {
+            name: role,
+            type: "input",
+            message: "Enter role"
+        },
+        {
+            name: manager,
+            type: "input",
+            message: "Enter manager"
+        }
+
+    ])
+    .then(answer)
+}
+
+//add roll - need add or update role?? - not sure how to (answer)
+function addRole() {
+    inquirer
+    .prompt([
+        {
+            name: role,
+            type: "input",
+            message: "Enter new role"
+        }
+    ])
+    .then(answer)
+
+//add department - need add or update department?? - not sure how to (answer)
+function addDepartment() {
+    inquirer
+    .prompt([
+        {
+            name: department,
+            type: "input",
+            message: "Enter new department"
+        }
+    ])
+    .then(answer)
+
+
+    function updateEmployee() {
+    inquirer
+    .prompt([
+        {
+            name: update,
+            type: "list", 
+            message: "What would you like to update?",
+            choices: [
+                "Department",
+                "Role",
+                "Manager"
+            ]
+            
+            }
+    ])
+    .then(answer)
+}
+    
+
+
+
+
+
+//function viewEmployee()
+
+//function viewDepartments()
+
+//function viewRole()
---
+++ schema.sql
@@ -0,0 +1,69 @@
+DROP DATABASE IF EXISTS track_empDB;
+CREATE track_empDB;
+
+USE track_empDB;
+
+CREATE TABLE employee (
+    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT(10),
+    manager_id INT(10),
+    PRIMARY KEY (employeeId)
+);
+
+
+CREATE TABLE role (
+    role_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT(10),
+    PRIMARY KEY (role_id)
+    
+);
+
+
+CREATE TABLE department (
+    department_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (department_id)
+    
+);
+
+INSERT INTO 
+	employee (first_name, last_name, role_id, manager_id)
+VALUES 
+	("Michael", "Scott", 1, 1),
+    ("Oscar", "Martinez", 2, 1),
+    ("Angela", "Martin", 3, 1),
+    ("Jim", "Halpert", 4, 1),
+    ("Dwight", "Schrute", 4, 1),
+    ("Creed", "Bratton", 5, 1),
+    ("Phyllis", "Vance", 6, 1),
+    ("Ryan", "Howard", 6, 1),
+    ("Pam", "Beesley", 7, 1);
+    
+    
+
+INSERT INTO 
+	role (title, salary, department_id)
+VALUES 
+	("Manager", 170000, 1),
+    ("Lawyer", 200000, 2),
+    ("Accounting", 85000, 3),
+    ("Sales", 100000, 4),
+    ("Engineer", 120000, 5),
+    ("Developer", 110000, 6),
+    ("Reception", 50000, 7);
+    
+   
+INSERT INTO 
+	department (name, id)
+VALUES 
+	("Manager", 1),
+    ("Lawyer", 2),
+    ("Accounting", 3),
+    ("Sales", 4),
+    ("Engineer", 5),
+    ("Developer", 6), 
+    ("Reception", 7);
